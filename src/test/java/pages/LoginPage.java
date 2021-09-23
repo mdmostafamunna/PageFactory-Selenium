6d31b45f0b4cdda928da4dc02058f36c5a5e5c83
MD Mostafa Munna <mdmostafamunna.sqa@gmail.com>
2021-09-23 19:32:47 +0600

    Adding @CacheLookup for storing locator in cache

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,19 +1,23 @@
 package pages;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class LoginPage extends DriverSetup {
 
 	@FindBy(id = "email")
+	@CacheLookup
 	WebElement emailLocator;
 	
 	
 	@FindBy(how = How.ID, using = "password")
+	@CacheLookup
 	WebElement passwordLocator;
 	
 	@FindBy(how = How.XPATH, using = "/html/body/div/div/div/div[1]/div/form/div[4]/div/button")
+	@CacheLookup
 	WebElement clickOnLoginBtnLocator;
 	
 	
